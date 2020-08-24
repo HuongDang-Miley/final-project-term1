714d33bd8a998d52fa2cbaff916efd02d07dcc6f
Huong Dang <miley.dawn@codeimmersives.com>
2020-08-24 15:11:49 -0400

    print shuffle question to the dom

--- back-end.js
+++ back-end.js
@@ -1,8 +1,4 @@
-
-
-// Get right answer by index 0
-const correctAnswer = questions[0].answer[0]
-console.log({ correctAnswer })
+// HELPER FUNCTIONS
 //Create a new array from the original arr, shuffle it
 function shuffle(a) {
     let result = [...a]
@@ -16,26 +12,108 @@ function shuffle(a) {
     return result
 }
 
-const shuffledAnswerArr = shuffle(questions[0].answer)
+// MAIN FUNCTIONS
 
-// function getCorrectAnswer(ans) {
-//     if (ans === correctAnswer) {
-//         return true
-//     } else {
-//         return false  
-//     }
-// }
+let currentQuestion = 0
+// const correctAnswer = questions[currentQuestion].answers[0]
+
+//load question when game start
+const startGame = function (currentQuestion) {
+    const objQuest = questions[currentQuestion]
+    // print question to question field
+    const printQuest = objQuest.question
+    document.querySelector('#question').innerText = printQuest
+    // shuffle answer, then print to choice field.
+    const shuffledAnswerArr = shuffle(objQuest.answers)
+    document.querySelector('#answer-a').innerText = shuffledAnswerArr[0]
+    document.querySelector('#answer-b').innerText = shuffledAnswerArr[1]
+    document.querySelector('#answer-c').innerText = shuffledAnswerArr[2]
+    document.querySelector('#answer-d').innerText = shuffledAnswerArr[3]
+}
+
+startGame(currentQuestion)
+
+// // When user Click: 1. show clicked option, 2/ show correct answer, 3/ show prize earn or game end
+
+// const prizeList = document.querySelectorAll('li')
+// const choices = document.querySelectorAll('.choice')
+
+// let totalQuestionPass = 15
+// let currentPrize = 0
+
+
+// // Run through choice of answers. If user click on any of them, add a class to change the button.
+// choices.forEach(function (choice) {
+//     choice.addEventListener('click', function (event) {
+//         event.target.classList.add("choice-clicked")
+//         if (event.target.innerText === correctAnswer) {
+//             setTimeout(function () {
+//                 // show correct answer in green
+//                 event.target.classList.replace("choice-clicked", "show-correct-answer")
+//                 // turn on correct-answer music
+//                 document.querySelector('iframe').src = './music/correct-answer.mp3'
+//                 // Update number of passed questions and add border to money tier
+//                 totalQuestionPass = totalQuestionPass - 1
+//                 console.log({ totalQuestionPass })
+//                 prizeList[totalQuestionPass].classList.add('pass-question')
+//                 // Update current prize
+//                 currentPrize = prizeArr[totalQuestionPass]
+//                 console.log({ currentPrize })
+//                 // Deactivate all button
+//                 document.querySelector('#answer-a').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-b').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-c').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-d').classList.add('deactivate-btn')
+//             }, 1000)
+//         }
+
+//         else {
+//             setTimeout(function () {
+//                 // show correct answer in green
+//                 document.querySelector('.correct-answer').classList.add('show-correct-answer')
+//                 // turn on correct-answer music
+//                 document.querySelector('iframe').src = './music/wrong-answer.mp3'
+//                 // deactivate all buttons
+//                 document.querySelector('#answer-a').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-b').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-c').classList.add('deactivate-btn')
+//                 document.querySelector('#answer-d').classList.add('deactivate-btn')
+//                 // Check current prize
+//                 console.log({ currentPrize })
+//                 console.log({ questionPass })
+//             }, 1000)
+//         }
+//     })
+// })
+
+
+
+// // console.log({ prize })
+// // console.log({ questionPass })
 
 
 
 
 
+//     // const correctAnswer = questions[0].answer[0]
 
-// console.log({ shuffledAnswerArr })
-// console.log(shuffledAnswerArr[0])
-// let test = getCorrectAnswer("1st")
-// console.log({test})
 
+// // Get right answer by index 0
+// // const shuffledAnswerArr = shuffle(questions[0].answer)
+
+// // function getCorrectAnswer(ans) {
+// //     if (ans === correctAnswer) {
+// //         return true
+// //     } else {
+// //         return false  
+// //     }
+// // }
+
+
+
+
+
+// // Helper function
 
 
 
--- question-list.js
+++ question-list.js
@@ -1,12 +1,12 @@
 // correct answer is at first index
 const questions =  [
     {
-        "question": "You're 3rd place right now in a race. What place are you in?",
-        answer: ["1st", "2nd", "3rd", "None"]
+        question: "You're 3rd place right now in a race. What place are you in when you pass the person in 2nd place?",
+        answers: ["1st", "2nd", "3rd", "None"]
     },
     {
         question: "How many months have 28 days?",
-        answer: ["All of them", "One", "Two", "Depends on if there’s a leap of year or no"]
+        answers: ["All of them", "One", "Two", "Depends on if there’s a leap of year or no"]
     },
     {
         "question": "The answer is really big",
